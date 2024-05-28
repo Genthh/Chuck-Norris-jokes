20609a41ab47a20448a6ba4d6c6ec1b7775bda57
Genthh <gentihulaj12@gmail.com>
2024-05-28 10:32:55 +0200

    changed endpoint

--- src/store/jokes.ts
+++ src/store/jokes.ts
@@ -2,7 +2,7 @@ import api from "@/utils/api";
 
 export const fetchJokes = async () => {
   try {
-    const response = await api.get("/fact");
+    const response = await api.get("/random");
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
