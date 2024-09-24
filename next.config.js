27bf678ec3cdcc2a18d74b0b5ff904016c464757
Genthh <101422592+Genthh@users.noreply.github.com>
2024-09-24 17:34:08 +0200

    new push

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const path = require("path");
+require("dotenv").config(); // This loads variables from the .env file
+
+const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
     API_URL_KEY: process.env.API_URL_KEY,
     API_URL_HOST: process.env.API_URL_HOST,
   },
 };
+
+module.exports = nextConfig;
